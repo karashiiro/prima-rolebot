0aa4db180c891af91ef1b9da392ef95ce96dd72f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-07-06 15:44:15 -0700

    add db and config

--- prima-rolebot.py
+++ prima-rolebot.py
@@ -1,13 +1,20 @@
+import configparser
 import discord
 import os
+import pymongo
 
 class Prima(discord.Client):
+	def __init__(self):
+		self.config = configparser.RawConfigParser();
+		self.config.read("config");
+		self.db = pymongo.MongoClient("mongodb://localhost:27017/")[self.config.get("database_name")];
+
 	async def on_ready(self):
-		print('Logged on as {0}!'.format(self.user))
+		print("Logged on as {0}!".format(self.user))
 
 	async def on_socket_raw_receive(self, message):
 		string = message[1]
 		print(string)
 
 client = Prima()
-client.run(os.environ['DISCORD_BOT_TOKEN'])
+client.run(os.environ["DISCORD_BOT_TOKEN"])
